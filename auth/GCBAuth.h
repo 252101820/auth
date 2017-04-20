b7905af5d7200283e4f74d5f88d1dde2b027a0eb
liuyuqing <liuyuqing@zeaho.com>
2017-04-20 13:59:05 +0800

    add pod files

---
+++ auth/GCBAuth.h
@@ -0,0 +1,13 @@
+//
+//  GCBAuth.h
+//  GCBJX
+//
+//  Created by 智鹤 on 16/11/14.
+//  Copyright © 2016年 智鹤. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GCBAuth : NSObject
+
+@end
---
+++ auth/GCBAuth.m
@@ -0,0 +1,13 @@
+//
+//  GCBAuth.m
+//  GCBJX
+//
+//  Created by 智鹤 on 16/11/14.
+//  Copyright © 2016年 智鹤. All rights reserved.
+//
+
+#import "GCBAuth.h"
+
+@implementation GCBAuth
+
+@end
